8ade29a209ed25d9150ecaa87d835b5038042d5b
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-05 23:38:28 +0200

    initial commit: added test.js with simple getaverage fucntion

---
+++ test.js
@@ -0,0 +1,12 @@
+function getAverage (a,b, c, d, e, f){   //funkcje zaczynamy jako function
+
+var average = (a + b + c + d + e + f)/2;
+console.log(average);
+return average;
+
+}    
+
+var myResult = getAverage(7,8,9,10,11,12);
+console.log("the average is" + myResult);
+
+
